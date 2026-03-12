af8b3aa4532d14320b1f09142def2b3644f968f6
AMAN UPADHYAY <upadhyayaman512@gmail.com>
2026-03-12 10:27:53 +0530

    Move backend files to backend directory

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB Connection Error: ', err.message));
 
